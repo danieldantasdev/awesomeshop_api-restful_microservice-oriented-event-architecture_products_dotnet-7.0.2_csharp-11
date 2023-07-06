e4987b78dbf85288f19b1948929d27c98ed5983d
danieldantasdev <contatolobinhodev@gmail.com>
2023-07-06 00:09:14 -0300

    bugfix: recursive class if class

--- AwesomeShop.Services.Products.Api/Controllers/ProductController.cs
+++ AwesomeShop.Services.Products.Api/Controllers/ProductController.cs
@@ -7,53 +7,50 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AwesomeShop.Services.Products.Api.Controllers;
 
-public class ProductController
+[Route("api/products")]
+[ApiController]
+public class ProductsController : ControllerBase
 {
-    [Route("api/products")]
-    [ApiController]
-    public class ProductsController : ControllerBase
+    private readonly IMediator _mediator;
+
+    public ProductsController(IMediator mediator)
     {
-        private readonly IMediator _mediator;
+        _mediator = mediator;
+    }
 
-        public ProductsController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var query = new GetAllProductsQueryInputModel();
 
-        [HttpGet]
-        public async Task<IActionResult> GetAll()
-        {
-            var query = new GetAllProductsQueryInputModel();
+        var result = await _mediator.Send(query);
 
-            var result = await _mediator.Send(query);
+        return Ok(result);
+    }
 
-            return Ok(result);
-        }
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var query = new GetProductByIdQueryInputModel(id);
 
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetById(Guid id)
-        {
-            var query = new GetProductByIdQueryInputModel(id);
+        var result = await _mediator.Send(query);
 
-            var result = await _mediator.Send(query);
+        return Ok(result);
+    }
 
-            return Ok(result);
-        }
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] AddProductCommandInputModel command)
+    {
+        var result = await _mediator.Send(command);
 
-        [HttpPost]
-        public async Task<IActionResult> Post([FromBody] AddProductCommandInputModel command)
-        {
-            var result = await _mediator.Send(command);
+        return Ok(result);
+    }
 
-            return Ok(result);
-        }
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(Guid id, [FromBody] UpdateProductCommandInputModel command)
+    {
+        var result = await _mediator.Send(command);
 
-        [HttpPut("{id}")]
-        public async Task<IActionResult> Put(Guid id, [FromBody] UpdateProductCommandInputModel command)
-        {
-            var result = await _mediator.Send(command);
-
-            return Ok(result);
-        }
+        return Ok(result);
     }
 }
